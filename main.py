16a358dbef256af81d624aa3dd3fe8e7215372e9
Bjorn Nilsson <bpnilsson@gmail.com>
2011-04-23 18:09:59 +0000

    110423 20:09

--- main.py
+++ main.py
@@ -1,41 +1,38 @@
 
-
+# python 3.2
 
 
 dict = { 0:[summary], 1:[], 2:[location, summary], 3:[desciption], 4:[summary], 5:[summary] }
 
 
 def main():
+    file = sys.argv[1]
+    new_file = sys.argv[2]
+    
     file = open(file, 'r')
     new_file = open(new_file, 'w')
-    
+
     while 1:
         line = file.readline()
-        
+
         if 'SUMMARY:' not in line:
-            new_file.write(file)
-            
+            new_file.write(line)
+
         else:
-            buffer = ''
-            summary = ''
-            location = ''
-            description = ''
-            
-            i = 0
-            
-            for char in line[7:]:
-                if char != '\n':
-                    buffer += char
-                    continue
-                
-                if buffer[0] == '-':
-                    buffer == ''
-                
-                for 
-                
-    
-    
-    
-        
-        
-        
+            summary     = []
+            location    = []
+            description = []
+
+            words = line[7:].split('\n')
+
+            for i, word in enum(words):
+                if word[0] == '-': word = ''
+
+                for list in dict[i]:
+                    list.append(word)
+
+            new_file.write('DESCRIPTION: ' + description)
+            new_file.write('LOCATION: '    + location)
+            new_file.write('SUMMARY: '     + ' '.join(summary))
+
+    new_file.close()
